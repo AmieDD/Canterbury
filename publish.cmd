d1c41d07172012783e8f607c9161baf7d3874cc2
AMIEDD\AmieDD <amie@amiedd.com>
2018-03-04 16:07:38 -0600

    Inital checkin for Canterbury chatbot for materials for making things

---
+++ publish.cmd
@@ -0,0 +1,3 @@
+nuget restore
+msbuild Microsoft.Bot.Sample.LuisBot.sln -p:DeployOnBuild=true -p:PublishProfile=canterbury-Web-Deploy.pubxml -p:Password=inBEL9rkxTZ4sLuE0l53tw97oKlAYKLY1GBDft5dcxHPupFyyXf610GWrpxq
+
